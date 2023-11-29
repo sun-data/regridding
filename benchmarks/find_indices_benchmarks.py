3da82bed802cb1299f4be44023e5d6de112c9701
Roy Smart <roytsmart@gmail.com>
2023-11-29 12:45:34 -0700

    Small updates to `find_indices()` benchmarks.

--- benchmarks/find_indices_benchmarks.py
+++ benchmarks/find_indices_benchmarks.py
@@ -21,5 +21,8 @@ def time_find_indices_1d(num: int, method: str):
 
 
 time_find_indices_1d.setup = setup_find_indices_1d
-time_find_indices_1d.params = ([10, 100, 1000, 10000], ["brute"])
+time_find_indices_1d.params = (
+    np.linspace(0, 1e4, num=11, dtype=int)[1:],
+    ["brute"],
+)
 time_find_indices_1d.param_names = ["num", "method"]

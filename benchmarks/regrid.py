0aee0f3fedc631a76a3c2076bcfefaa6f1e0373f
Roy Smart <roytsmart@gmail.com>
2023-11-29 20:53:07 -0700

    Improved benchmarks for `regridding.regrid()` function.

---
+++ benchmarks/regrid.py
@@ -0,0 +1,70 @@
+import numpy as np
+import regridding
+
+vertices_input = None
+vertices_output = None
+values_input = None
+
+
+def setup_regrid_2d(
+    shape: tuple[int, int],
+    method: str,
+    order: int,
+):
+    num_x, num_y = shape
+
+    x = np.linspace(-1, 1, num=num_x)
+    y = np.linspace(-1, 1, num=num_y)
+    x, y = np.meshgrid(x, y, indexing="ij")
+
+    angle = 0.4
+    x_input = x * np.cos(angle) - y * np.sin(angle) + 0.05 * x * x
+    y_input = x * np.sin(angle) + y * np.cos(angle) + 0.05 * y * y
+
+    x_output = np.linspace(x_input.min(), x_input.max(), num=num_x)
+    y_output = np.linspace(y_input.min(), y_input.max(), num=num_y)
+    x_output, y_output = np.meshgrid(x_output, y_output, indexing="ij")
+
+    global vertices_input
+    global vertices_output
+    global values_input
+
+    vertices_input = (x_input, y_input)
+    vertices_output = (x_output, y_output)
+    values_input = np.random.uniform(0, 1, size=(num_x - 1, num_y - 1))
+
+    regridding.regrid(
+        vertices_input=vertices_input,
+        vertices_output=vertices_output,
+        values_input=values_input,
+        method=method,
+        order=order,
+    )
+
+
+def time_regrid_2d(
+    shape: tuple[int, int],
+    method: str,
+    order: int,
+):
+    regridding.regrid(
+        vertices_input=vertices_input,
+        vertices_output=vertices_output,
+        values_input=values_input,
+        method=method,
+        order=order,
+    )
+
+
+time_regrid_2d.setup = setup_regrid_2d
+time_regrid_2d.params = (
+    [(100, 100), (200, 200), (300, 300), (400, 400)],
+    ["conservative"],
+    [1],
+)
+time_regrid_2d.param_names = (
+    "shape",
+    "method",
+    "order",
+)
+time_regrid_2d.timeout = 480

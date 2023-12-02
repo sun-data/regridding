29d7c3c7773fe149974d3f3e989899f0f529f2bd
Roy Smart <roytsmart@gmail.com>
2023-12-01 17:08:47 -0700

    Added Google analytics id to the documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -86,6 +86,9 @@ html_theme_options = {
             "icon": "fa-brands fa-python",
         },
     ],
+    "analytics": {
+        "google_analytics_id": "G-4B4VQVE4HN"
+    }
 }
 
 # https://github.com/readthedocs/readthedocs.org/issues/2569

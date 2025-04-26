5e9fcb6d7540a23b90b7f3e1f88422e60744779b
Roy Smart <roytsmart@gmail.com>
2025-04-26 16:54:44 -0500

    Added `negate_3d()`, `sum_3d()`, `difference_3d()`, `multiply_3d()`, `dot_3d()`, and `cross_3d()` to the `regridding.math` module. (#16)

--- regridding/math.py
+++ regridding/math.py
@@ -1,7 +1,17 @@
+"""
+Compiled versions of fundamental mathematical functions.
+"""
+
 import numba
 
 __all__ = [
     "sign",
+    "negate_3d",
+    "sum_3d",
+    "difference_3d",
+    "multiply_3d",
+    "dot_3d",
+    "cross_3d",
 ]
 
 
@@ -16,3 +26,141 @@ def sign(x: float):
         the value to find the sign of
     """
     return bool(x > 0) - bool(x < 0)
+
+
+@numba.njit(cache=True, inline="always", error_model="numpy")
+def negate_3d(
+    a: tuple[float, float, float],
+) -> tuple[float, float, float]:
+    r"""
+    Compute :math:`-a` where :math:`a` is a 3D vector.
+
+    Parameters
+    ----------
+    a
+        A 3D vector.
+    """
+    x, y, z = a
+
+    return -x, -y, -z
+
+
+@numba.njit(cache=True, inline="always", error_model="numpy")
+def sum_3d(
+    a: tuple[float, float, float],
+    b: tuple[float, float, float],
+) -> tuple[float, float, float]:
+    r"""
+    Compute the sum :math:`a + b` between two 3D vectors :math:`a`
+    and :math:`b`.
+
+    Parameters
+    ----------
+    a
+        A 3D vector.
+    b
+        Another 3D vector.
+    """
+    x_a, y_a, z_a = a
+    x_b, y_b, z_b = b
+
+    x = x_a + x_b
+    y = y_a + y_b
+    z = z_a + z_b
+
+    return x, y, z
+
+
+@numba.njit(cache=True, inline="always", error_model="numpy")
+def difference_3d(
+    a: tuple[float, float, float],
+    b: tuple[float, float, float],
+) -> tuple[float, float, float]:
+    r"""
+    Compute the difference :math:`a - b` between two 3D vectors :math:`a`
+    and :math:`b`.
+
+    Parameters
+    ----------
+    a
+        A 3D vector.
+    b
+        Another 3D vector.
+    """
+    x_a, y_a, z_a = a
+    x_b, y_b, z_b = b
+
+    x = x_a - x_b
+    y = y_a - y_b
+    z = z_a - z_b
+
+    return x, y, z
+
+
+@numba.njit(cache=True, inline="always", error_model="numpy")
+def multiply_3d(
+    r: float,
+    a: tuple[float, float, float],
+) -> tuple[float, float, float]:
+    """
+    Multiply a 3D vector :math:`a` by a scalar :math:`r`.
+
+    Parameters
+    ----------
+    r
+        Scalar operand.
+    a
+        Vector operand.
+    """
+    x, y, z = a
+
+    return r * x, r * y, r * z
+
+
+@numba.njit(cache=True, inline="always", error_model="numpy")
+def dot_3d(
+    a: tuple[float, float, float],
+    b: tuple[float, float, float],
+) -> float:
+    r"""
+    Compute the scalar product :math:`a \cdot b` between two 3D vectors :math:`a`
+    and :math:`b`.
+
+    Parameters
+    ----------
+    a
+        A 3D vector.
+    b
+        Another 3D vector.
+    """
+    x_a, y_a, z_a = a
+    x_b, y_b, z_b = b
+
+    return x_a * x_b + y_a * y_b + z_a * z_b
+
+
+@numba.njit(cache=True, inline="always", error_model="numpy")
+def cross_3d(
+    a: tuple[float, float, float],
+    b: tuple[float, float, float],
+) -> tuple[float, float, float]:
+    r"""
+    Compute the vector product :math:`a \times b` between two 3D vectors :math:`a`
+    and :math:`b`.
+
+    Parameters
+    ----------
+    a
+        A 3D vector.
+    b
+        Another 3D vector.
+    """
+
+    x_a, y_a, z_a = a
+    x_b, y_b, z_b = b
+
+    x = +(y_a * z_b - z_a * y_b)
+    y = -(x_a * z_b - z_a * x_b)
+    z = +(x_a * y_b - y_a * x_b)
+
+    return x, y, z
--- regridding/tests/test_math.py
+++ regridding/tests/test_math.py
@@ -23,3 +23,97 @@ def test_sign(
 ):
     result = regridding.math.sign(x)
     assert result == result_expected
+
+
+@pytest.mark.parametrize(
+    argnames="a,result_expected",
+    argvalues=[
+        ((1, 1, 1), (-1, -1, -1)),
+        ((-1, 1, 1), (1, -1, -1)),
+    ],
+)
+def test_negate_3d(
+    a: tuple[float, float, float],
+    result_expected: tuple[float, float, float],
+):
+    result = regridding.math.negate_3d(a)
+    assert result == result_expected
+
+
+@pytest.mark.parametrize(
+    argnames="a,b,result_expected",
+    argvalues=[
+        ((1, 1, 1), (-1, -1, -1), (0, 0, 0)),
+        ((1, 1, 1), (1, 1, 1), (2, 2, 2)),
+    ],
+)
+def test_sum_3d(
+    a: tuple[float, float, float],
+    b: tuple[float, float, float],
+    result_expected: tuple[float, float, float],
+):
+    result = regridding.math.sum_3d(a, b)
+    assert result == result_expected
+
+
+@pytest.mark.parametrize(
+    argnames="a,b,result_expected",
+    argvalues=[
+        ((1, 1, 1), (-1, -1, -1), (2, 2, 2)),
+        ((1, 1, 1), (1, 1, 1), (0, 0, 0)),
+    ],
+)
+def test_difference_3d(
+    a: tuple[float, float, float],
+    b: tuple[float, float, float],
+    result_expected: tuple[float, float, float],
+):
+    result = regridding.math.difference_3d(a, b)
+    assert result == result_expected
+
+
+@pytest.mark.parametrize(
+    argnames="r,a,result_expected",
+    argvalues=[
+        (2, (1, 1, 1), (2, 2, 2)),
+    ],
+)
+def test_multiply_3d(
+    r: float,
+    a: tuple[float, float, float],
+    result_expected: tuple[float, float, float],
+):
+    result = regridding.math.multiply_3d(r, a)
+    assert result == result_expected
+
+
+@pytest.mark.parametrize(
+    argnames="a,b,result_expected",
+    argvalues=[
+        ((1, 1, 1), (1, 1, 1), 3),
+        ((1, 1, 1), (2, 2, 2), 6),
+    ],
+)
+def test_dot_3d(
+    a: tuple[float, float, float],
+    b: tuple[float, float, float],
+    result_expected: float,
+):
+    result = regridding.math.dot_3d(a, b)
+    assert result == result_expected
+
+
+@pytest.mark.parametrize(
+    argnames="a,b,result_expected",
+    argvalues=[
+        ((1, 0, 0), (0, 1, 0), (0, 0, 1)),
+        ((0, 1, 0), (1, 0, 0), (0, 0, -1)),
+    ],
+)
+def test_cross_3d(
+    a: tuple[float, float, float],
+    b: tuple[float, float, float],
+    result_expected: float,
+):
+    result = regridding.math.cross_3d(a, b)
+    assert result == result_expected

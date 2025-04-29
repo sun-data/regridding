bafd4ce696a06120b6f0150feb48cb717c1f0dbe
Roy Smart <roytsmart@gmail.com>
2025-04-28 22:31:52 -0600

    Added support for 3D conservative resampling to `regridding.weights()`.

---
+++ regridding/_weights/_weights_conservative_3d_test.py
@@ -0,0 +1,79 @@
+import pytest
+import numpy as np
+from . import _weights_conservative_3d
+
+_x = np.arange(3)
+_y = np.arange(4)
+_z = np.arange(5)
+
+_x, _y, _z = np.meshgrid(_x, _y, _z, indexing="ij")
+
+
+@pytest.mark.parametrize(
+    argnames="grid_input,grid_output",
+    argvalues=[
+        (
+            np.meshgrid(
+                np.arange(2),
+                np.arange(2),
+                np.arange(2),
+                indexing="ij",
+            ),
+            np.meshgrid(
+                np.arange(2),
+                np.arange(2),
+                np.arange(2),
+                indexing="ij",
+            ),
+        )
+    ]
+)
+def test_weights_conservative_3d(
+    grid_input: tuple[np.ndarray, np.ndarray, np.ndarray],
+    grid_output: tuple[np.ndarray, np.ndarray, np.ndarray],
+):
+    result = _weights_conservative_3d.weights_conservative_3d(
+        grid_input=grid_input,
+        grid_output=grid_output,
+    )
+
+
+@pytest.mark.parametrize(
+    argnames="grid,result_expected",
+    argvalues=[
+        (
+            np.meshgrid(
+                np.arange(2),
+                np.arange(2),
+                np.arange(2),
+                indexing="ij",
+            ),
+            np.ones((1, 1, 1))
+        ),
+        (
+            np.meshgrid(
+                np.arange(3),
+                np.arange(4),
+                np.arange(5),
+                indexing="ij",
+            ),
+            np.ones((2, 3, 4))
+        ),
+        (
+            np.meshgrid(
+                2 * np.arange(3),
+                2 * np.arange(4),
+                2 * np.arange(5),
+                indexing="ij",
+            ),
+            8 * np.ones((2, 3, 4))
+        ),
+    ]
+)
+def test_cell_volume(
+    grid: tuple[np.ndarray, np.ndarray, np.ndarray],
+    result_expected: np.ndarray,
+):
+    result = _weights_conservative_3d._cell_volume(grid)
+    assert np.allclose(result, result_expected)
+    assert result.shape == result_expected.shape

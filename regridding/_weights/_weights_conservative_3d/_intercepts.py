b708151d909890b5b0969e707d59d32873e9aeac
Roy Smart <roytsmart@gmail.com>
2025-05-02 13:56:19 -0600

    Added `_intercepts` module

---
+++ regridding/_weights/_weights_conservative_3d/_intercepts.py
@@ -0,0 +1,216 @@
+"""
+Create lists of intercepts generated during axis sweeps and determine their
+contribution to the final list of weights.
+"""
+
+import numpy as np
+import numba
+import regridding as rg
+from . import _arrays
+from . import _volumes
+from . import _grids
+
+
+@numba.njit(cache=True)
+def empty(
+    shape_input: tuple[int, int, int],
+    shape_output: tuple[int, int, int],
+) -> numba.typed.List:
+    """
+    Create an empty list of intercepts between each plane in the
+    input grid and each plane in the output grid.
+
+    There are three planes in the input grid and thre
+
+    Parameters
+    ----------
+    shape_input
+        The shape of the input grid.
+    shape_output
+        The shape of the output grid.
+    """
+    intercepts = numba.typed.List()
+    for a in range(len(shape_input)):
+        intercepts_a = numba.typed.List()
+        for b in range(len(shape_output)):
+            intercepts_ab = numba.typed.List()
+            for i in range(shape_input[a]):
+                intercepts_abi = numba.typed.List()
+                for j in range(shape_output[b]):
+                    intercepts_abij = numba.typed.List()
+                    for x in range(0):
+                        intercepts_abij.append((0, 0, (0.0, 0.0, 0.0)))
+                    intercepts_abi.append(intercepts_abij)
+                intercepts_ab.append(intercepts_abi)
+            intercepts_a.append(intercepts_ab)
+        intercepts.append(intercepts_a)
+    return intercepts
+
+
+@numba.njit(cache=True)
+def sweep(
+    intercepts: numba.typed.List,
+    weights: numba.typed.List,
+    grid_input: tuple[np.ndarray, np.ndarray, np.ndarray],
+    grid_output: tuple[np.ndarray, np.ndarray, np.ndarray],
+    volume_input: np.ndarray,
+) -> None:
+    """
+    Sweep through the list of intercepts and add the volume of each
+    segment to the list of weights.
+
+    Parameters
+    ----------
+    intercepts
+        A sequence of intercepts and the corresponding indices in the
+        input/output grids.
+    weights
+        The current list of weights.
+        New weights will be appended to this list.
+    grid_input
+        The vertices of the input grid.
+    grid_output
+        The vertices of the output grid.
+    volume_input
+        The volume of each cell in the input grid.
+    """
+
+    x_input, y_input, z_input = grid_input
+    x_output, y_output, z_output = grid_output
+
+    shape_input = x_input.shape
+    shape_output = x_output.shape
+
+    shape_centers_input = _grids.shape_centers(shape_input)
+    shape_centers_output = _grids.shape_centers(shape_output)
+
+    for a in range(len(intercepts)):
+
+        intercepts_a = intercepts[a]
+
+        normal_a = [1 if axis == a else 0 for axis in range(len(intercepts))]
+
+        for b in range(len(intercepts_a)):
+
+            intercepts_ab = intercepts_a[b]
+
+            normal_b = [1 if axis == b else 0 for axis in range(len(intercepts))]
+
+            for i in range(len(intercepts_ab)):
+
+                intercepts_abi = intercepts_ab[i]
+
+                for j in range(len(intercepts_abi)):
+
+                    intercepts_abij = intercepts_abi[j]
+
+                    if not intercepts_abij:
+                        continue
+
+                    i0_input, i0_output, p0 = intercepts_abij[0]
+
+                    i0_input = _arrays.index_3d(i0_input, shape_input)
+                    i0_output = _arrays.index_3d(i0_output, shape_output)
+
+                    for t in range(1, len(intercepts_abij)):
+
+                        i1_input, i1_output, p1 = intercepts_abij[t]
+
+                        i1_input = _arrays.index_3d(i1_input, shape_input)
+                        i1_output = _arrays.index_3d(i1_output, shape_output)
+
+                        i0_input_lower = rg.math.difference_3d(i0_input, normal_a)
+                        i0_input_upper = i0_input
+
+                        i0_output_lower = rg.math.difference_3d(i0_output, normal_b)
+                        i0_output_upper = i0_output
+
+                        apex_input = (
+                            x_input[i0_input],
+                            y_input[i0_input],
+                            z_input[i0_input],
+                        )
+                        apex_output = (
+                            x_output[i0_output],
+                            y_output[i0_output],
+                            z_output[i0_output],
+                        )
+
+                        vol_input = _volumes.volume_tetrahedron(apex_input, p0, p1)
+                        vol_output = _volumes.volume_tetrahedron(apex_output, p0, p1)
+
+                        volume_input_lower = volume_input[i0_input_lower]
+                        volume_input_upper = volume_input[i0_output_upper]
+
+                        input_lower_in_bounds = _arrays.index_in_bounds(
+                            index=i0_input_lower,
+                            shape=shape_centers_input,
+                        )
+                        input_upper_in_bounds = _arrays.index_in_bounds(
+                            index=i0_input_upper,
+                            shape=shape_centers_input,
+                        )
+                        output_lower_in_bounds = _arrays.index_in_bounds(
+                            index=i0_output_lower,
+                            shape=shape_centers_output,
+                        )
+                        output_upper_in_bounds = _arrays.index_in_bounds(
+                            index=i0_output_upper,
+                            shape=shape_centers_output,
+                        )
+
+                        i0_input_lower = _arrays.index_flat(
+                            index=i0_input_lower,
+                            shape=shape_input,
+                        )
+                        i0_input_upper = _arrays.index_flat(
+                            index=i0_input_upper,
+                            shape=shape_input,
+                        )
+
+                        i0_output_lower = _arrays.index_flat(
+                            index=i0_output_lower,
+                            shape=shape_output,
+                        )
+                        i0_output_upper = _arrays.index_flat(
+                            index=i0_output_upper,
+                            shape=shape_output,
+                        )
+
+                        if input_lower_in_bounds:
+                            if output_lower_in_bounds:
+                                weight_lower_lower = (
+                                    i0_input_lower,
+                                    i0_output_lower,
+                                    (-vol_input - vol_output) / volume_input_lower,
+                                )
+                                weights.append(weight_lower_lower)
+
+                            if output_upper_in_bounds:
+                                weight_lower_upper = (
+                                    i0_input_lower,
+                                    i0_output_upper,
+                                    (-vol_input + vol_output) / volume_input_lower,
+                                )
+                                weights.append(weight_lower_upper)
+
+                        if input_upper_in_bounds:
+                            if output_lower_in_bounds:
+                                weight_upper_lower = (
+                                    i0_input_upper,
+                                    i0_output_lower,
+                                    (vol_input - vol_output) / volume_input_upper,
+                                )
+                                weights.append(weight_upper_lower)
+
+                            if output_upper_in_bounds:
+                                weight_upper_upper = (
+                                    i0_input_upper,
+                                    i0_output_upper,
+                                    (vol_input + vol_output) / volume_input_upper,
+                                )
+                                weights.append(weight_upper_upper)
+
+                        p0 = p1
+                        i0_input = i1_input
+                        i0_output = i1_output
---
+++ regridding/_weights/_weights_conservative_3d/_intercepts_test.py
@@ -0,0 +1,64 @@
+import pytest
+import numpy as np
+import numba
+from . import _intercepts
+
+
+@pytest.mark.parametrize(
+    argnames="grid_input,grid_output,volume_input",
+    argvalues=[
+        (
+            np.meshgrid(
+                np.arange(3),
+                np.arange(4),
+                np.arange(5),
+                indexing="ij",
+            ),
+            np.meshgrid(
+                np.arange(3) + 0.5,
+                np.arange(4) + 0.5,
+                np.arange(5) + 0.5,
+                indexing="ij",
+            ),
+            np.ones((2, 3, 4)),
+        )
+    ],
+)
+def test_sweep(
+    grid_input: tuple[np.ndarray, np.ndarray, np.ndarray],
+    grid_output: tuple[np.ndarray, np.ndarray, np.ndarray],
+    volume_input: np.ndarray,
+):
+
+    weights = _test_sweep_compiled(grid_input, grid_output, volume_input)
+
+    assert len(weights) > 0
+
+
+@numba.njit
+def _test_sweep_compiled(
+    grid_input: tuple[np.ndarray, np.ndarray, np.ndarray],
+    grid_output: tuple[np.ndarray, np.ndarray, np.ndarray],
+    volume_input: np.ndarray,
+):
+    intercepts = _intercepts.empty(
+        shape_input=grid_input[0].shape,
+        shape_output=grid_output[0].shape,
+    )
+
+    intercepts[0][0][0][0].append((0, 0, (0.05, 0.15, 0.35)))
+    intercepts[0][0][0][0].append((0, 1, (0.85, 0.25, 0.25)))
+
+    weights = numba.typed.List()
+    for _ in range(0):
+        weights.append((0, 0, 0.))
+
+    _intercepts.sweep(
+        intercepts=intercepts,
+        weights=weights,
+        grid_input=grid_input,
+        grid_output=grid_output,
+        volume_input=volume_input,
+    )
+
+    return weights

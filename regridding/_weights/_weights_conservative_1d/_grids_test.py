56b7a09948f7f9e2f284222c41d15e670523b5c3
Roy Smart <roytsmart@gmail.com>
2026-02-10 20:40:02 -0700

    black

--- regridding/_weights/_weights_conservative_1d/_grids_test.py
+++ regridding/_weights/_weights_conservative_1d/_grids_test.py
@@ -2,15 +2,15 @@ import pytest
 import numpy as np
 from . import _grids
 
-
 grid = np.arange(5)
+
 
 @pytest.mark.parametrize(
     argnames="point,grid,result_expected",
     argvalues=[
         (0.5, grid, 1),
         (3.5, grid, 4),
-    ]
+    ],
 )
 def test_index_of_point(
     point: float,

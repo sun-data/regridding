f2380d83d4b645cce81e65ea556e4ebc3d93549d
Roy Smart <roytsmart@gmail.com>
2026-02-10 20:38:31 -0700

    added tests for grid

---
+++ regridding/_weights/_weights_conservative_1d/_grids_test.py
@@ -0,0 +1,24 @@
+import pytest
+import numpy as np
+from . import _grids
+
+
+grid = np.arange(5)
+
+@pytest.mark.parametrize(
+    argnames="point,grid,result_expected",
+    argvalues=[
+        (0.5, grid, 1),
+        (3.5, grid, 4),
+    ]
+)
+def test_index_of_point(
+    point: float,
+    grid: np.ndarray,
+    result_expected: int,
+):
+    result = _grids.index_of_point(
+        point=point,
+        grid=grid,
+    )
+    assert result == result_expected

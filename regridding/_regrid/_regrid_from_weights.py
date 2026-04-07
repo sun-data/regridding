lots of fixes to broadcasting

regridding/_regrid/_regrid_from_weights.py
@@ -54,25 +54,43 @@ def regrid_from_weights(
 
     unit = getattr(values_input, "unit", None)
 
-    shape_input = np.broadcast_shapes(values_input.shape, shape_input)
-
     ndim_input = len(shape_input)
-    axis_input = _util._normalize_axis(axis_input, ndim=ndim_input)
+    ndim_output = len(shape_output)
 
-    shape_orthogonal = (
+    axis_input = _util._normalize_axis(axis_input, ndim=ndim_input)
+    axis_output = _util._normalize_axis(axis_output, ndim=ndim_output)
+
+    shape_input_orthogonal = tuple(
         shape_input[i] for i in range(-len(shape_input), 0) if i not in axis_input
     )
+    shape_output_orthogonal = tuple(
+        shape_output[i] for i in range(-len(shape_output), 0) if i not in axis_output
+    )
+
+    shape_orthogonal = np.broadcast_shapes(
+        shape_input_orthogonal,
+        shape_output_orthogonal,
+    )
+
+    axis_input = tuple(sorted(axis_input))
+    axis_output = tuple(sorted(axis_output))
+
+    shape_input_new = list(reversed(shape_orthogonal))
+    for ax in reversed(axis_input):
+        shape_input_new.insert(~ax, shape_input[ax])
+    shape_input = list(reversed(shape_input_new))
+
+    shape_output_new = list(reversed(shape_orthogonal))
+    for ax in reversed(axis_output):
+        shape_output_new.insert(~ax, shape_output[ax])
+    shape_output = list(reversed(shape_output_new))
+
+    shape_input = np.broadcast_shapes(values_input.shape, shape_input)
+
     weights = np.broadcast_to(np.array(weights), shape_orthogonal, subok=True)
     values_input = np.broadcast_to(values_input, shape_input, subok=True)
 
     if values_output is None:
-        shape_output = np.broadcast_shapes(
-            shape_output,
-            tuple(
-                shape_input[ax] if ax not in axis_input else 1
-                for ax in _util._normalize_axis(None, ndim_input)
-            ),
-        )
         values_output = np.zeros_like(values_input, shape=shape_output)
     else:
         if values_output.shape != shape_output:  # pragma: nocover
@@ -80,9 +98,6 @@ def regrid_from_weights(
                 f"{values_output.shape=} should be equal to {shape_output}"
             )
         values_output.fill(0)
-
-    ndim_output = len(shape_output)
-    axis_output = _util._normalize_axis(axis_output, ndim=ndim_output)
 
     axis_input_numba = ~np.arange(len(axis_input))[::-1]
     axis_output_numba = ~np.arange(len(axis_output))[::-1]

regridding/_weights/_weights_conservative_1d/_weights_conservative_1d_test.py
@@ -4,13 +4,14 @@ import regridding
 
 
 @pytest.mark.parametrize(
-    "x_input, x_output, values_input, values_expected, axis",
+    "x_input, x_output, values_input, values_expected, axis_input, axis_output",
     [
         (
             np.linspace(-1, 1, num=11),
             np.linspace(-1, 1, num=6),
             np.ones(10),
             2 * np.ones(5),
+            None,
             None,
         ),
         (
@@ -19,12 +20,14 @@ import regridding
             np.ones(10),
             2 * np.ones(5),
             None,
+            None,
         ),
         (
             np.linspace(-1, 1, num=11),
             np.linspace(-1, 1, num=6) - 1e-6,
             np.ones(10),
             2 * np.ones(5),
+            None,
             None,
         ),
         (
@@ -33,6 +36,7 @@ import regridding
             np.ones(10),
             np.ones(20) / 2,
             0,
+            ~0,
         ),
         (
             np.linspace(-1, 1, num=11),
@@ -40,6 +44,7 @@ import regridding
             np.ones(10),
             2 * np.ones(5),
             ~0,
+            0,
         ),
         (
             np.linspace(1, -1, num=11),
@@ -47,12 +52,24 @@ import regridding
             np.ones(10),
             2 * np.ones(5),
             (0,),
+            0,
         ),
         (
             np.broadcast_to(np.linspace(1, -1, num=11), (3, 4, 11)),
             np.linspace(1, -1, num=6),
             np.ones(10),
             2 * np.ones(5),
+            ~0,
+            0,
+        ),
+        (
+            np.broadcast_to(
+                np.linspace(1, -1, num=11)[..., np.newaxis, np.newaxis], (11, 3, 4)
+            ),
+            np.linspace(1, -1, num=6),
+            np.ones(10)[..., np.newaxis, np.newaxis],
+            2 * np.ones(5),
+            0,
             ~0,
         ),
         (
@@ -61,6 +78,7 @@ import regridding
             np.ones(10),
             2 * np.ones(5),
             ~0,
+            ~0,
         ),
         (
             np.linspace(-1, 1, num=11),
@@ -68,6 +86,15 @@ import regridding
             np.ones(10),
             0,
             None,
+            None,
+        ),
+        (
+            np.linspace(1, -1, num=11),
+            np.linspace(1, -1, num=6),
+            np.broadcast_to(np.ones(10), shape=(3, 4, 10)),
+            2 * np.ones(5),
+            ~0,
+            ~0,
         ),
     ],
 )
@@ -76,14 +103,15 @@ def test_regrid_conservative_1d(
     x_output: np.ndarray | tuple[np.ndarray],
     values_input: np.ndarray,
     values_expected: np.ndarray,
-    axis: None | int | tuple[int],
+    axis_input: None | int | tuple[int],
+    axis_output: None | int | tuple[int],
 ):
     values_output = regridding.regrid(
         coordinates_input=x_input,
         coordinates_output=x_output,
         values_input=values_input,
-        axis_input=axis,
-        axis_output=axis,
+        axis_input=axis_input,
+        axis_output=axis_output,
         method="conservative",
     )
 